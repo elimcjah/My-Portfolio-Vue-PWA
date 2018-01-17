c6314e1613a8992f5a0391553f7318bcf19ac932
Elijah McClendon <Elijah@fungry.com>
2018-01-17 16:50:37 -0700

    Feature (002) Make NavBar Component

---
+++ src/components/AppNavBar.vue
@@ -0,0 +1,52 @@
+<template>
+<header>
+  <nav class="menu">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+</template>
+
+<script>
+  export default {
+    name: 'app-nav-bar',
+  };
+</script>
+
+<style scoped>
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #fff;
+}
+</style>
