84cc9b4c01ef885e7f18c524994e3f75d0bec342
Elijah McClendon <Elijah@fungry.com>
2018-01-17 16:55:31 -0700

    Feature (002) Create NavBar Component

--- src/components/AppNavBar.vue
+++ src/components/AppNavBar.vue
@@ -2,9 +2,9 @@
 <header>
   <nav class="menu">
     <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
+      <li><a href="#" target="_blank" rel="noopener">Home</a></li>
+      <li><a href="#" target="_blank" rel="noopener">About</a></li>
+      <li><a href="#" target="_blank" rel="noopener">Contact</a></li>
     </ul>
   </nav>
 </header>
